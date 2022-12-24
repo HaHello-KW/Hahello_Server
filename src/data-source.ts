a10bcffece68e7abbaed2d9b01c5aa7ebe5333c1
SeungHo0422 <songseungho9258@gmail.com>
2022-12-25 03:26:07 +0900

    [Feature/DB] DB connection 코드 작성
    DB connection 코드 작성 및 수정

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,26 +1,22 @@
 import 'reflect-metadata';
-import { ConnectionOptions, createConnection } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import { User } from './entity/Users';
+import { DataSource } from 'typeorm';
 
-const dataSource = async (): Promise<void> => {
-  try {
-    const connectionOption: ConnectionOptions = {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      synchronize: true,
-      logging: false,
-      entities: ['src/entity/**/*.ts'],
-      migrations: ['src/migration/**/*.ts'],
-      subscribers: ['src/subscriber/**/*.ts'],
-      namingStrategy: new SnakeNamingStrategy(),
-    };
-    await createConnection(connectionOption);
-  } catch (err) {
-    console.log('Connection Error \n', err);
-  }
-};
+export const MySQLDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  namingStrategy: new SnakeNamingStrategy(),
+  /**
+   * namingStrategy
+   * : Naming strategy to be used to name tables and columns in the DB.
+   */
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import * as express from 'express';
 import { Request, Response, NextFunction, Application } from 'express';
+import { MySQLDataSource } from './data-source';
+import { Users } from './entity/Users';
 
 const app: Application = express();
 const prod: boolean = process.env.NODE_ENV === 'production';
+app.use(express.json());
 
+// DB connection
+MySQLDataSource.initialize()
+  .then(() => {
+    console.log('MySQL DataSource has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during MySQL DataSource Initialization', err);
+  });
+
+// Register Routes
 app.get('/', (req, res) => {
   res.send('Initialized KEET server');
 });
 
+// Server Listening
 app.listen(prod ? process.env.PORT : 3065, () => {
   console.log(`KEET-server is running on PORT:${process.env.PORT}`);
 });
