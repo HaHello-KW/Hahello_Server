2e8e97e4ca1a03529ef9eb01ca77e5392f4888cf
SeungHo0422 <songseungho9258@gmail.com>
2023-03-24 04:31:11 +0900

    ♻️ Refactor index code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,62 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 import * as express from 'express';
-import { Request, Response, NextFunction, Application } from 'express';
-import { json } from 'sequelize';
+import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as expressSession from 'express-session';
+// import * as passport from 'passport';
+import * as hpp from 'hpp';
+import * as Helmet from 'helmet';
+
 import { MySQLDataSource } from './data-source';
 import Test_Controller from './controllers/test_Controller';
 
 // Controllers (route handlers)
 // import * as userController from './controllers/userController';
 
-const app: Application = express();
+dotenv.config();
+const app = express();
 const prod: boolean = process.env.NODE_ENV === 'production';
+
+app.set('port', prod ? process.env.PORT : 3065);
+
+// Middleware
+if (prod) {
+  // app.use(Helmet());
+  app.use(hpp());
+  app.use(
+    cors({
+      origin: /hola\.com$/,
+      credentials: true,
+    })
+  );
+} else {
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+}
+
+app.use('/', express.static('uploads'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: false, //https -> true
+      domain: prod ? '.hola.com' : undefined,
+    },
+    name: 'rnbck',
+  })
+);
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // DB connection
 MySQLDataSource.initialize()
@@ -23,13 +68,11 @@ MySQLDataSource.initialize()
   });
 
 // Register Routes
-app.get('/', (req, res) => {
-  res.send('Initialized Hahello server');
+app.get('/', (req, res, next) => {
+  res.send('Initialized Hola server');
 });
 
-// app.get('/user', userController.(   ));
-
 // Server Listening
-app.listen(prod ? process.env.PORT : 3065, () => {
-  console.log(`Hahello-server is running on PORT:${process.env.PORT}`);
+app.listen(app.get('port'), () => {
+  console.log(`Hola-server is running on PORT: ${app.get('port')}`);
 });
